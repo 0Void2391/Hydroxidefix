752dc76d28de572926ee951b643c2b4fdd37b905
0_Void <106687374+0Void2391@users.noreply.github.com>
2024-09-04 11:06:02 +0200

    Update MessageBox.lua

--- ui/controls/MessageBox.lua
+++ ui/controls/MessageBox.lua
@@ -1,6 +1,6 @@
 local TextService = game:GetService("TextService")
 
-local Interface = import("rbxassetid://11389137937")
+local Interface = import("ui/ui")
 local Base = Interface.Base
 local Object = Base.MessageBox
 local Shadow = Base.MessageBoxShadow
